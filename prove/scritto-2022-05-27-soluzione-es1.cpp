22adc92c14b3ca252a8aa6cca613d50f82335068
Flecart <63465494+Flecart@users.noreply.github.com>
2022-06-20 21:08:28 +0000

    Deploying to gh-pages from @ csunibo/programmazione@36240068d63c4f55757edb99d555a45625e704ee 🚀

--- prove/scritto-2022-05-27-soluzione-es1.cpp
+++ prove/scritto-2022-05-27-soluzione-es1.cpp
@@ -1,17 +1,30 @@
-// La funzione da chiamare è revert, che internamente chiama la funzione ricorsiva i_revert
+// NOTA: LE REGION MARCATE DEBUG SONO PER 
+// TESTARE IL CODICE NON SONO NECESSARIE NEL COMPITO
+// REGION DEBUG 
+#include <bits/stdc++.h>
+using namespace std; 
 
-void revert(nodo arr[], int lun) {
-  bool swap = false;
-  revert_i(arr, lun, &swap);
+struct nodo {
+    int val;
+    int next;
+};
+
+void swap(int *p, int *d) {
+    int tmp = *p; 
+    *p = *d; 
+    *d = tmp; 
 }
+// ENDREGION DEBUG
 
-int i_revert(nodo arr[], int lun, bool * noswap, int j=0) {
+// REGION ESERCIZIO 1
+
+int i_revert(nodo arr[], int lun, bool *noswap, int j=0) {
   if(arr[j].next == -1) {
     swap(&arr[0].val, &arr[j].val);
     return arr[0].next;
   } else {
     int index = i_revert(arr, lun, noswap, arr[j].next);
-    if(j == arr[index].next || index == j) *noswap = true;
+    if(arr[j].next == index || index == j) *noswap = true;
     if(!(*noswap)) {
       swap(&arr[j].val, &arr[index].val);
       return arr[index].next;
@@ -19,3 +32,34 @@ int i_revert(nodo arr[], int lun, bool * noswap, int j=0) {
     return 0;
   }
 }
+
+// La funzione da chiamare è revert, che internamente chiama la funzione ricorsiva i_revert
+void revert(nodo arr[], int lun) {
+  bool swap = false;
+  i_revert(arr, lun, &swap);
+}
+
+// REGION DEBUG 
+ostream &operator <<(ostream &out, nodo n) {
+    out << "(" << n.val << ", " << n.next << ") ";
+    return out; 
+}
+
+int main() {
+    nodo test_arr[] = {
+        {1, 1},
+        {2, 2},
+        {3, 3},
+        {4, -1}
+    };
+    for (auto &i : test_arr) cout << i;
+    cout << endl; 
+
+    revert(test_arr, 4);
+
+    for (auto &i : test_arr) cout << i;
+    cout << endl; 
+
+    // TODO: se ti va fai altri test
+}
+// ENDREGION DEBUG 
